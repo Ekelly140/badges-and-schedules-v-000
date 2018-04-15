fc9bd46ddbdf781b90f69bcfef320405f4898298
ERIC KELLY <eakelly220@gmail.com>
2018-04-15 17:36:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,11 @@ def batch_badge_creator(name)
 end 
 
 def assign_rooms(names)
-  
-  
+newArray = [] 
+count =1
+ names.each do |name| 
+   newArray << "Hello, #{name}! You'll be assigned to room #{count}!"
+   count ++
+end 
+  return newArray
 end 
